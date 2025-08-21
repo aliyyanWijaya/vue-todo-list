4649387399226abaa782660eec0d0fe03b7a063e
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-08-22 10:55:28 +1200

    First commit

---
+++ todo-vue/src/App.vue
@@ -0,0 +1,74 @@
+<script>
+import ToDoItem from './components/ToDoItem.vue';
+import ToDoForm from "./components/ToDoForm.vue";
+
+export default {
+  name: "app",
+  components: {
+    ToDoItem,
+    ToDoForm,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { label: "Learn Vue", done: false },
+        { label: "Create a Vue project with the CLI", done: true },
+        { label: "Have fun", done: true },
+        { label: "Create a to-do list", done: false },
+      ],
+    };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({
+        label: toDoLabel,
+        done: false,
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item 
+        :label="item.label" 
+        :done="item.done"
+        :id="item.id"></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
